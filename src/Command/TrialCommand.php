33ad61ef3eebadcb481071929a83395393457ea3
FerHigher <fernandohiguero@hotmail.com>
2021-12-16 21:23:12 +0100

    TrialCommand first version

---
+++ src/Command/TrialCommand.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Contract;
+use App\Entity\Trial;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:Trial',
+    description: 'Add a short description for your command',
+)]
+class TrialCommand extends Command
+{
+    // Todo inyectar el Trial en el constructor del comando para poder usarlo luego
+   /* private Trial $trial;
+
+    public function __construct(Trial $trial)
+    {
+        parent::__construct();
+        $this->trial = $trial;
+    }
+*/
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::REQUIRED, 'Plaintiff signatures')
+            ->addArgument('arg2', InputArgument::REQUIRED, 'Defendant signatures')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+        $plaintiff = new Contract('Plaintiff', $arg1);
+        $arg2 = $input->getArgument('arg2');
+        $defendant = new Contract('Defendant', $arg2);
+
+        $trial = new Trial();
+
+        $trial->getVerdict($plaintiff, $defendant);
+
+        if ($arg1) {
+            $io->note(sprintf('You passed Plaintiff signatures: %s', $arg1));
+        }
+        if ($arg2) {
+            $io->note(sprintf('You passed Defendant signatures: %s', $arg2));
+        }
+
+//        if ($input->getOption('option1')) {
+//            // ...
+//        }
+        return Command::SUCCESS;
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        echo "Initializing Trial...\n\r";
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $arg1 = $input->getArgument('arg1');
+        $arg2 = $input->getArgument('arg2');
+
+        if (!$this->validateSignature($arg1)) {
+            $output->writeln("Max length: 3 signatures");
+            return Command::INVALID;
+        }
+
+
+    }
+
+    private function validateSignature($arg1)
+    {
+        $length = strlen($arg1);
+        if ($length > 3) {
+            return false;
+        }
+        return true;
+    }
+
+
+}
