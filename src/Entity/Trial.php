868e3c0dc0e67b113b3992ed67695696823ae59f
FerHigher <fernandohiguero@hotmail.com>
2021-12-16 23:47:17 +0100

    Some logic is still left

--- src/Entity/Trial.php
+++ src/Entity/Trial.php
@@ -17,4 +17,22 @@ class Trial
         }
     }
 
+    public function needMore(Contract $hashed, Contract $complete): int
+    {
+
+        if ($hashed->getTotalValue() > $complete->getTotalValue()) {
+            // entonces ya ha ganado aun teniendo un hash
+            return -1;
+        }
+        if ($hashed->getTotalValue() == $complete->getTotalValue()) {
+            return 0;
+        }
+        if ($hashed->getTotalValue() < $complete->getTotalValue()) {
+            return $complete->getTotalValue() - $hashed->getTotalValue();
+        }
+
+        // TODO falta decir que firmas serían necesarias para ganar pq ademas habria que comprobar la presencia a no de rey en la firma,
+        // en ese caso una V no valdria en el teorico caso de que haga falta un punto para ganar. En ese caso haría falta una N, que son dos puntos.
+    }
+
 }
