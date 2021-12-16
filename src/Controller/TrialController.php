89c970e0e2613c7ee61c5ceca373680f8729ad53
FerHigher <fernandohiguero@hotmail.com>
2021-12-16 23:47:57 +0100

    Controller without dependency injection

--- src/Controller/TrialController.php
+++ src/Controller/TrialController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contract;
+use App\Entity\Trial;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,52 +17,22 @@ class TrialController extends AbstractController
      */
     public function trial(Request $request): JsonResponse
     {
-        $plaintiff = new Contract('plaintiff', $request->query->get('plaintiff'), false, false);
-        $defendant = new Contract('defendant', $request->query->get('defendant'), false, false);
+        $plaintiff = new Contract('plaintiff', $request->query->get('plaintiff'));
+        $defendant = new Contract('defendant', $request->query->get('defendant'));
 
-        $this->getVerdict($plaintiff->getTotalValue(), $defendant->getTotalValue());
+        $trial = new Trial(); // Todo: pelear la inyeccion de dependencia
+        $trial->getVerdict($plaintiff, $defendant);
 
         if ($plaintiff->getHasHash()) {
-            $diff = $this->needMore($plaintiff, $defendant);
-            dump('Plaintiff at least needs ' . ($diff +1) . ' points to win this trial');
+            $diff = $trial->needMore($plaintiff, $defendant);
+            dump('Plaintiff at least needs ' . ($diff + 1) . ' points to win this trial');
         }
-        if($defendant->getHasHash()){
-            $diff = $this->needMore($defendant, $plaintiff);
-            dump('Defendant at least needs ' . ($diff +1) . ' points to win this trial');
+        if ($defendant->getHasHash()) {
+            $diff = $trial->needMore($defendant, $plaintiff);
+            dump('Defendant at least needs ' . ($diff + 1) . ' points to win this trial');
         }
 
         return new JsonResponse(['status' => 'trial finished', Response::HTTP_OK]);
-
     }
 
-    public function getVerdict(int $plaintiff, int $defendant): void
-    {
-        if ($plaintiff > $defendant) {
-            dump('Plaintiff (' . $plaintiff . ' points) wins the trial over defendant (' . $defendant . ' points)');
-        }
-        if ($defendant > $plaintiff) {
-            dump('Defendant (' . $defendant . ' points) wins the trial over plaintiff (' . $plaintiff . ' points)');
-        }
-        if ($defendant == $plaintiff) {
-            dump('It\'s a draw. The score was ' . $plaintiff . ' all ');
-        }
-    }
-
-    private function needMore(Contract $hashed, Contract $complete): int
-    {
-
-        if($hashed->getTotalValue() > $complete->getTotalValue()){
-            // entonces ya ha ganado aun teniendo un hash
-            return -1;
-        }
-        if($hashed->getTotalValue() == $complete->getTotalValue()){
-            return 0;
-        }
-        if($hashed->getTotalValue() < $complete->getTotalValue()){
-            return $complete->getTotalValue() - $hashed->getTotalValue();
-        }
-
-        // TODO falta decir que firmas serían necesarias para ganar pq ademas habria que comprobar la presencia a no de rey en la firma,
-        // en ese caso una V no valdria en el teorico caso de que haga falta un punto para ganar. En ese caso haría falta una N, que son dos puntos.
-    }
 }
