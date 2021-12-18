af5032f5a81b2aa921b0d5218e902bfa460e7230
FerHigher <fernandohiguero@hotmail.com>
2021-12-18 15:54:20 +0100

    conjecture added to JsonResponse

---
+++ README.md
@@ -0,0 +1,20 @@
+## Requirements
+
+PHP 8 and later.
+
+## How to set up the project
+
+You can install the libraries via [Composer](http://getcomposer.org/). Run the following command:
+
+```bash
+composer install
+```
+
+## Execute through endpoint
+
+
+## Execute using command line 
+
+```bash
+php bin/console app:Trial NNN NNK
+```
--- src/Controller/TrialController.php
+++ src/Controller/TrialController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Contract;
-use App\Entity\Trial;
+use App\Model\Contract;
+use App\Model\Trial;
+use App\View\TrialResultView;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,24 +16,45 @@ class TrialController extends AbstractController
     /**
      * @Route ("/trial", name="trial", methods={"GET"})
      */
-    public function trial(Request $request): JsonResponse
+    public function trial(Request $request, Trial $trial): JsonResponse
     {
         $plaintiff = new Contract('plaintiff', $request->query->get('plaintiff'));
         $defendant = new Contract('defendant', $request->query->get('defendant'));
 
-        $trial = new Trial(); // Todo: pelear la inyeccion de dependencia
-        $trial->getVerdict($plaintiff, $defendant);
 
-        if ($plaintiff->getHasHash()) {
+        $winner = $trial->getVerdict($plaintiff, $defendant);
+
+        $trialResult = new TrialResultView();
+        $message = $trialResult->prepareVerdict($winner, $plaintiff, $defendant);
+
+
+        $conjecture = $this->hashedNeeds($plaintiff, $trial, $defendant);
+
+        return new JsonResponse(['Status' => 'Trial finished',
+            'Verdict' => $message,
+            'To win:' => $conjecture
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * @param Contract $plaintiff
+     * @param Trial $trial
+     * @param Contract $defendant
+     * @return string
+     */
+    public function hashedNeeds(Contract $plaintiff, Trial $trial, Contract $defendant): string
+    {
+        $message = '';
+        if ($plaintiff->getHasHash() && !$defendant->getHasHash()) {
             $diff = $trial->needMore($plaintiff, $defendant);
-            dump('Plaintiff at least needs ' . ($diff + 1) . ' points to win this trial');
+            $message = sprintf('Plaintiff at least needs %s points to win this trial', ($diff + 1));
         }
         if ($defendant->getHasHash()) {
             $diff = $trial->needMore($defendant, $plaintiff);
-            dump('Defendant at least needs ' . ($diff + 1) . ' points to win this trial');
+            $message = sprintf('Defendant at least needs %s points to win this trial', ($diff + 1));
         }
 
-        return new JsonResponse(['status' => 'trial finished', Response::HTTP_OK]);
+        return $message;
     }
 
 }
