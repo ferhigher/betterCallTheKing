0ab35d84ebed65e1072515ef21effb308a4ed9bc
FerHigher <fernandohiguero@hotmail.com>
2021-12-20 00:20:13 +0100

    format

--- src/View/TrialResultView.php
+++ src/View/TrialResultView.php
@@ -11,7 +11,7 @@ class TrialResultView
         return match ($winner) {
             -1 => sprintf('Plaintiff (%s points) wins the trial over defendant (%s points).', $plaintiff->getTotalValue(), $defendant->getTotalValue()),
             1 => sprintf('Defendant (%s points) wins the trial over plaintiff (%s points).', $defendant->getTotalValue(), $plaintiff->getTotalValue()),
-            0 => sprintf('It\'s a draw. The score was %s all.', $plaintiff->getTotalValue()),
+            0 => sprintf('It is a draw. The score was %s all.', $plaintiff->getTotalValue()),
             default => '',
         };
     }
